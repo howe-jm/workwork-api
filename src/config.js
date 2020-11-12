31bc89e3c953957910f9af17ec569c3fd09f9824
Jason Howe <trisynth@gmail.com>
2020-11-11 21:10:16 -0500

    Preparation for initial deployment.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://howe-jm@localhost/workwork',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://username@localhost/workwork',
   TEST_DATABASE_URL:
-    process.env.TEST_DATABASE_URL || 'postgresql://howe-jm@localhost/workwork-test',
+    process.env.TEST_DATABASE_URL || 'postgresql://username@localhost/workwork-test',
 };
